ae6851544c5b977329e112caf1e754ee6fba9e7d
reut ohayon <66826644+reut201112@users.noreply.github.com>
2021-02-12 17:45:34 +0200

    devisces list component

---
+++ chillers-FE-UI/src/Pages/CounterDevices/CounterDevices.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DevicesList from '../../Components/DevicesList/DevicesList';
+
+
+const CounterDevices=()=>{
+    return(
+    <div>
+    <DevicesList />
+    </div>
+    );
+};
+
+export default CounterDevices;
