43f30b710580fab294b3ad31ef9f2188558f229b
MORIA COHAVI <cohavimoria@gmail.com>
2021-03-31 22:16:12 +0300

    =create countersService

--- chillers-BE-MODBUS/src/server.js
+++ chillers-BE-MODBUS/src/server.js
@@ -1,29 +1,9 @@
-const express = require('express');
-const app = express();
 const myDbConnection = require('./db/database'); // connection to db
-//const chillersService = require('./services/chillersService');
+const chillersService = require('./services/chillersService');
 //const countersService = require('./services/countersService');
 const countersService = require('./services/tempService');
-const counters = require('./utils/constants/counters');
-const PORT = process.env.PORT || 3200;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
-app.use(express.json());
 
 
-setInterval(function(){
-  countersService({'host': '172.16.11.194', 'name': 'name'});
-}, 5000);
-
-// var cs;
-// var countersInstances = [];
-
-// cs = new countersService({'host': '172.16.11.194', 'name': 'name'});
-// cs.initialModbusConnection();
-
-// setInterval(function(){
-//   cs.connectToCounter();
-// }, 5000);
-
+countersService({'host': '172.16.11.218', 'name': 'cellcom'});
+countersService({'host': '172.16.11.217', 'name': 'pelephone'});
+countersService({'host': '172.16.11.219', 'name': 'orange'});
--- chillers-BE-MODBUS/src/services/countersService.js
+++ chillers-BE-MODBUS/src/services/countersService.js
@@ -1,88 +1,113 @@
-const net = require('net');
 const Modbus = require('jsmodbus');
+const net = require('net');
+const mongoose = require('mongoose');
+const { countersSchema } = require('../utils/schemas');
+const DBobj = mongoose.model('counters', countersSchema, 'counters');
+const myDbConnection = require('../db/database'); // connection to db
 
-var self;
 class CountersService{
     constructor(counter){
-        this.options = {
-            'host': counter.host,
-            'port': 502
-        };
-        console.log(counter.host);
-        this.name = counter.name;
-        this.unitId = 1;
-        self = this;
-        this.initialModbusConnection();
+      console.log("constructor");
+      this.options = {
+          'host': counter.host,
+          'port': 502
+      };
+      this.name = counter.name;
+      this.unitId = 1;
     }
 
     initialModbusConnection(){
-        this.socket = new net.Socket();
-        this.client = new Modbus.client.TCP(this.socket, this.unitId);
-        // what will happen on error 
-        this.socket.on('error', (error)=> { this.onError(error) });
-        // what will happen on timeout 
-        //this.socket.on('timeout', this.onTimeout);
-        // what will happen on close 
-        this.socket.on('end', this.onEnd);
-        this.socket.on('close', this.onClose);
-        // what will happen on connect
-        this.socket.on('data', this.onData);
-        this.socket.on('connect', this.onConnect);
+      console.log("initial");
+      this.socket = new net.Socket();
+      this.client = new Modbus.client.TCP(this.socket, this.unitId);
+
+      this.socket.setTimeout(10000);
+      this.socket.connect(this.options);
+
+      this.socket.on('connect', () => {this.onConnect()})
+
+      this.socket.on('end', () => {this.onEnd()});
+      
+      this.socket.on('data', (data) => {this.onData(data)});
+      
+      this.socket.on('close', () => {this.onClose()});
+      
+      this.socket.on('error', (error) => {this.onError(error)});
+
+      this.socket.on('timeout', () => {this.onTimeout()});
     }
 
-    onTimeout = function(){
-      console.log('on timeout ' + self.name);
+    onTimeout(){
+      console.log('on timeout');
+      this.socket.destroy();
     }
 
-    onEnd = function(){
-      console.log('on end ' + self.name);
+    onEnd(){
+      console.log('on end');
     }
   
-    onData = function(data){
-      self.socket.write('Server Reply: ' + data);
+    onData(data){
+      this.socket.write('Server Reply: ' + data);
     }
   
-    onError = function(error){
-      console.log('on error ' + self.name);
+    onError(error){
       console.log(error);
-      self.socket.end('socket can send some more data but it will be ended');
+      this.socket.end('socket can send some more data but it will be ended');
     }
 
-    onClose = function(){
+    onClose(){
       console.log('on close');
-      // setTimeout(() => {
-      //   console.log('Reconnecting..');
-      //   self.socket.connect(self.options);
-      // }, 5000);
-    }
-
-    connectToCounter(){
-      console.log('connecting to ' + this.name);
+      console.log('Reconnecting..');
       this.socket.connect(this.options);
     }
 
-    onConnect = async function() {
-      const client = self.client;
-      try {
-        console.log('on connect');
-        const c1 = client.readHoldingRegisters(7537, 2);
-        const c2 = client.readHoldingRegisters(7136, 2);
-        const c3 = client.readHoldingRegisters(7138, 2);
-        const c4 = client.readHoldingRegisters(7539, 2);
-        const c5 = client.readHoldingRegisters(7140, 2);
-        const c6 = client.readHoldingRegisters(273, 2);
- 
-        Promise.all([c1, c2, c3, c4, c5, c6]).then((values) => {
-          values.map(value => {
-            console.log(value.response._body._values);
-          });
-        }).catch(error => {
-          console.log(error);
+    connectToCounter(){
+      this.socket.setTimeout(10000);
+      this.socket.connect(this.options);
+    }
+
+    async onConnect() {
+      console.log("connect")
+      const c1 = this.client.readHoldingRegisters(7537, 2);
+      const c2 = this.client.readHoldingRegisters(7136, 2);
+      const c3 = this.client.readHoldingRegisters(7138, 2);
+      const c4 = this.client.readHoldingRegisters(7539, 2);
+      const c5 = this.client.readHoldingRegisters(7140, 2);
+      const c6 = this.client.readHoldingRegisters(273, 2);
+  
+      var data = [];
+      let dataToStore;
+  
+      Promise.all([c1, c2, c3, c4, c5, c6]).then(async (values) => {
+        values.map(value => {
+          value.response._body._values.map(val => data.push(val));
         });
-      }
-      catch(e) {
-        console.log('An error occured when tried to get / insert new data from chiller.', e.response._body);
-      }
+  
+        dataToStore = {
+          dateTime: new Date(),
+          i1: data[0],
+          i2: data[1],  
+          i3: data[3],
+          n_v1: data[4],
+          n_v2: data[5],
+          n_v3: data[6],
+          v1_v2: data[7],
+          v1_v3: data[8],
+          v2_v3: data[9],
+          cos: data[10],
+          counterName: this.name
+        }
+  
+        if(dataToStore){
+          console.log(dataToStore);
+          const counterData = new DBobj(dataToStore);
+          await counterData.save();
+          console.log('SAVED_DATA');
+          this.socket.destroy();
+        }
+      }).catch(error => {
+        console.log(error);
+      });
     }
 }
 
