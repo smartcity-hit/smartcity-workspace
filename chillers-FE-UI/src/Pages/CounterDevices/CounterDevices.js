4ff8061c8d33e1fbd069cf22d7d835e37761502f
Reut Ohayon <66826644+reut201112@users.noreply.github.com>
2021-04-10 16:28:54 +0300

    add new counter in counter->devices

--- chillers-FE-UI/src/Pages/CounterDevices/CounterDevices.js
+++ chillers-FE-UI/src/Pages/CounterDevices/CounterDevices.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import DevicesList from '../../Components/DevicesList/DevicesList';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCounters } from '../../redux/Counters-List/counters-list-actions';
+import AddDevice from '../../Components/AddDevice/AddDevice';
 import "./CounterDevices.scss"; 
 
 const CounterDevices = () => {
   const counters = useSelector((state) => state.countersList.counters);
   const dispatch = useDispatch();
 
+  const [activeTab, setActiveTab] = useState(0);
+
+
+	const onClickTab = (tabClicked) => {
+		setActiveTab(tabClicked);
+	  }
 
   useEffect(() => {
     dispatch(getCounters());
@@ -32,14 +39,25 @@ const CounterDevices = () => {
       minWidth: 100,
       align: 'center',
     },
+    
   ];
 
   return (
-    <div className="body">
-      
-      <h1 className="counter-title">COUNTERS</h1>
-      <DevicesList rows={counters} cols={columns} />
-    
+    <div className="admin-panel-wrapper">
+    <div className="admin-panel-container">
+      <h1 className='admin-panel-title'>COUNTERS</h1>
+        <div className='tabs'>
+          <div className={`tab ${activeTab === 0 ? 'active' : ''}`} onClick={() => { onClickTab(0) }}>
+            <label className="tab-name">View Counters</label>
+          </div>
+          <div className={`tab ${activeTab === 1 ? 'active' : ''}`} onClick={() => { onClickTab(1) }}>
+            <label className="tab-name">Add Counter</label>
+          </div>
+        </div>
+         </div>
+      <div className='active-tab'>
+      {activeTab === 0 ? <DevicesList rows={counters} cols={columns} /> : activeTab === 1 ? <AddDevice deviceTypeId={2}/> : ''}
+      </div>
     </div>
   );
 };
