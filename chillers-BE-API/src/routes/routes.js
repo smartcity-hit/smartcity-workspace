daaa85a41ba08a9a6624be8866338e3f51c29b17
KoralMazor <koralmazor@gmail.com>
2021-04-10 19:23:38 +0300

    delete devices route and api

--- chillers-BE-API/src/routes/routes.js
+++ chillers-BE-API/src/routes/routes.js
@@ -4,5 +4,4 @@ const router = new Router();
 router.use('/users', require('./api/users'));
 router.use('/chillers', require('./api/chillers'));
 router.use('/counters', require('./api/counters'));
-router.use('/devices', require('./api/devices'));
 module.exports = router;
