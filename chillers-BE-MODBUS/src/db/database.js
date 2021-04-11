b4d1ecd088f0eee500e8639f2e97ae4b05467c9f
MORIA COHAVI <cohavimoria@gmail.com>
2021-04-11 23:21:07 +0300

    =create health check service for counters

--- chillers-BE-MODBUS/src/db/database.js
+++ chillers-BE-MODBUS/src/db/database.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 const { loadMongooseModels } = require('../services/dbService');
-const { loadMongooseModels } = require('../utils/schema');
 
 async function myDbConnection() {
 
--- chillers-BE-MODBUS/src/server.js
+++ chillers-BE-MODBUS/src/server.js
@@ -2,7 +2,8 @@ const myDbConnection = require('./db/database'); // connection to db
 const chillersService = require('./services/chillersService');
 //const countersService = require('./services/countersService');
 const countersService = require('./services/tempService');
-
+const pi = require('./services/countersHealthCheckService');
+pi.countersHealthCheck();
 
 countersService({'host': '172.16.11.218', 'name': 'cellcom'});
 countersService({'host': '172.16.11.217', 'name': 'pelephone'});
